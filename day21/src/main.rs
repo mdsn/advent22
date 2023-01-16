c12ea5cb9416c5daf60b4fce2a27a309aa58ba1c
mdsn <madasbytes@gmail.com>
2023-01-15 22:00:39 -0500

    Solve day 21 part 2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::io::prelude::*;
 
 #[derive(Debug)]
@@ -21,6 +21,14 @@ impl Monkey {
             Monkey::Add(a, b) | Monkey::Sub(a, b) | Monkey::Mul(a, b) | Monkey::Div(a, b) => {
                 a == name || b == name
             }
+        }
+    }
+
+    fn operands(&self) -> [&String; 2] {
+        assert!(!self.is_k());
+        match self {
+            Monkey::Add(a, b) | Monkey::Sub(a, b) | Monkey::Mul(a, b) | Monkey::Div(a, b) => [a, b],
+            _ => unreachable!(),
         }
     }
 }
@@ -51,35 +59,106 @@ fn main() {
         })
         .collect();
 
-    // root: pppw + sjmn
-    // dbpl: 5
-    // cczh: sllz + lgvd
-    // zczc: 2
-    // ptdq: humn - dvpt
-    // dvpt: 3
-    // lfqf: 4
-    // humn: 5
-    // ljgn: 2
-    // sjmn: drzm * dbpl
-    // sllz: 4
-    // pppw: cczh / lfqf
-    // lgvd: ljgn * ptdq
-    // drzm: hmdt - zczc
-    // hmdt: 32
+    let starters: HashSet<String> = monkeys
+        .iter()
+        .filter_map(|(name, op)| op.is_k().then_some(name.to_string()))
+        .collect();
+    let queue = toposort(&monkeys, starters, "root");
+    let part1 = yell(&monkeys, queue);
+    dbg!(part1);
 
-    let queue = toposort(&monkeys);
-    let mut numbers: HashMap<String, u64> = [].into();
-    for name in queue {
-        let monkey = &monkeys[&name];
+    let (lq, rq) = split_branches(&monkeys, "root");
+    let (human_branch, monkey_branch) = if lq.contains(&"humn".to_string()) {
+        (lq, rq)
+    } else {
+        (rq, lq)
+    };
+
+    let part2 = reverse_engineer(&monkeys, human_branch, yell(&monkeys, monkey_branch));
+    dbg!(part2);
+}
+
+fn reverse_engineer(monkeys: &HashMap<String, Monkey>, queue: Vec<String>, target: u64) -> u64 {
+    // Which of the two branches is the known operand
+    enum KnownOp {
+        L,
+        R,
+    }
+
+    assert!(!queue.is_empty());
+    let root = queue.last().unwrap();
+    let (lq, rq) = split_branches(monkeys, root);
+    let (human_branch, monkey_branch, known) = if lq.contains(&"humn".to_string()) {
+        (lq, rq, KnownOp::R)
+    } else {
+        (rq, lq, KnownOp::L)
+    };
+
+    let known_operand = yell(monkeys, monkey_branch);
+    let root_monkey = &monkeys[root];
+    assert!(!root_monkey.is_k());
+
+    let op = match root_monkey {
+        Monkey::Add(_, _) => Op::Add,
+        Monkey::Sub(_, _) => match known {
+            KnownOp::R => Op::Subr,
+            KnownOp::L => Op::Subl,
+        },
+        Monkey::Mul(_, _) => Op::Mul,
+        Monkey::Div(_, _) => match known {
+            KnownOp::R => Op::Divr,
+            KnownOp::L => Op::Divl,
+        },
+        _ => unreachable!(),
+    };
+
+    let new_target = reverse_op(op, target, known_operand);
+    if human_branch.len() == 1 {
+        // only humn left
+        new_target
+    } else {
+        reverse_engineer(monkeys, human_branch, new_target)
+    }
+}
+
+fn split_branches(monkeys: &HashMap<String, Monkey>, root: &str) -> (Vec<String>, Vec<String>) {
+    let [left, right] = monkeys[root].operands();
+    let lq = toposort(monkeys, find_starters(monkeys, left), left);
+    let rq = toposort(monkeys, find_starters(monkeys, right), right);
+    (lq, rq)
+}
+
+#[derive(Debug)]
+enum Op {
+    Add,
+    Mul,
+    Divl, // known operand on left
+    Divr, // known operand on right
+    Subl, // known operand on left
+    Subr, // known operand on right
+}
+
+fn reverse_op(op: Op, target: u64, operand: u64) -> u64 {
+    match op {
+        Op::Add => target - operand,  // Add(x, 3) = 10 -> 10 - 3 = 7
+        Op::Mul => target / operand,  // Mul(2, x) = 16 -> 16 / 2 = 8
+        Op::Divl => operand / target, // Divl(12, x) = 3 -> 12 / 3 = 4
+        Op::Divr => target * operand, // Divr(x, 4) = 3  -> 3 * 4 = 12
+        Op::Subl => operand - target, // Subl(5, x) = 2  -> 5 - 2 = 3
+        Op::Subr => target + operand, // Subr(x, 3) = 9  -> 9 + 3 = 12
+    }
+}
+
+fn yell(monkeys: &HashMap<String, Monkey>, queue: Vec<String>) -> u64 {
+    let root = &queue.last().unwrap();
+    let mut numbers: HashMap<&String, u64> = [].into();
+    for name in &queue {
+        let monkey = &monkeys[name];
         if let Monkey::K(k) = monkey {
             numbers.insert(name, *k);
         } else {
-            let (a, b) = match monkey {
-                Monkey::Add(a, b) | Monkey::Sub(a, b) | Monkey::Mul(a, b) | Monkey::Div(a, b) => {
-                    (numbers[a], numbers[b])
-                }
-                _ => unreachable!(),
-            };
+            let [a, b] = monkey.operands();
+            let (a, b) = (numbers[a], numbers[b]);
             let k = match monkey {
                 Monkey::Add(_, _) => a + b,
                 Monkey::Sub(_, _) => a - b,
@@ -90,35 +169,57 @@ fn main() {
             numbers.insert(name, k);
         }
     }
-    dbg!(&numbers["root"]);
+    numbers[*root]
 }
 
-fn toposort(monkeys: &HashMap<String, Monkey>) -> Vec<String> {
-    let mut queue = vec![];
-    let mut starter: HashSet<&String> = monkeys
-        .iter()
-        .filter_map(|(name, op)| op.is_k().then_some(name))
-        .collect();
+fn find_starters(monkeys: &HashMap<String, Monkey>, root: &str) -> HashSet<String> {
+    let mut starters: HashSet<String> = [].into();
+    let mut queue: VecDeque<&str> = [root].into();
+    while !queue.is_empty() {
+        let name = queue.pop_front().unwrap().to_string();
+        let monkey = &monkeys[&name];
 
+        if monkey.is_k() {
+            starters.insert(name);
+            continue;
+        }
+
+        for op in &monkey.operands() {
+            if monkeys[*op].is_k() {
+                starters.insert(op.to_string());
+            } else {
+                queue.push_back(*op);
+            }
+        }
+    }
+    starters
+}
+
+fn toposort(
+    monkeys: &HashMap<String, Monkey>,
+    mut starters: HashSet<String>,
+    root: &str,
+) -> Vec<String> {
+    let mut queue = vec![];
     loop {
-        let ready = starter.iter().next().cloned().unwrap();
-        starter.remove(ready);
+        let ready = starters.iter().next().cloned().unwrap();
+        starters.remove(&ready);
         queue.push(ready.clone());
 
-        if ready == "root" {
+        if ready == root {
             break;
         }
 
         let (name, monkey) = monkeys
             .iter()
-            .find(|(_, monkey)| monkey.needs(ready))
+            .find(|(_, monkey)| monkey.needs(&ready))
             .unwrap();
 
         assert!(!monkey.is_k());
         match monkey {
             Monkey::Add(a, b) | Monkey::Sub(a, b) | Monkey::Mul(a, b) | Monkey::Div(a, b) => {
                 if queue.contains(a) && queue.contains(b) {
-                    starter.insert(name);
+                    starters.insert(name.to_string());
                 }
             }
             Monkey::K(_) => unreachable!(),
